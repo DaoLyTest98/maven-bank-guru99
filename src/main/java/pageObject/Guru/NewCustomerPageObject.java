5c8031eeb9c66b6edb4d913761bcd84c8fe8ecd9
DaoLyTest98 <daolytest@gmail.com>
2023-11-02 19:54:11 +0900

    New Customer

--- src/main/java/pageObject/Guru/NewCustomerPageObject.java
+++ src/main/java/pageObject/Guru/NewCustomerPageObject.java
@@ -1,5 +1,9 @@
 package pageObject.Guru;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -7,50 +11,117 @@ import commons.BasePage;
 import pageUIs.Guru.CommonPageUI;
 import pageUIs.Guru.NewCustomerPageUI;
 
-public class NewCustomerPageObject extends BasePage{
+public class NewCustomerPageObject extends BasePage {
 	private WebDriver driver;
+
 	public NewCustomerPageObject(WebDriver driver) {
 		this.driver = driver;
 	}
+
 	public String getTextTitle() {
 		waitForElementVisible(driver, NewCustomerPageUI.TITLE_TEXT);
 		return getElementText(driver, NewCustomerPageUI.TITLE_TEXT);
 	}
+
 	public boolean isDisplaysTextLabel(String name) {
 		waitForElementVisible(driver, CommonPageUI.DYNAMIC_LABEL_TEXT, name);
 		return isElementDisplayed(driver, CommonPageUI.DYNAMIC_LABEL_TEXT, name);
 	}
+
 	public void inputToDynamicTextbox(WebDriver driver, String fielName, String inputValue) {
 		waitForElementVisible(driver, CommonPageUI.DYNAMIC_TEXTBOX, fielName);
 		if (fielName.equals("Date of Birth")) {
-			removeAttributeInDOM(driver, CommonPageUI.DYNAMIC_TEXTBOX,"type");
+			removeAttributeInDOM(driver, CommonPageUI.DYNAMIC_TEXTBOX, "type");
 			sleepInSecond(2);
 		}
 		sendkeyToElement(driver, CommonPageUI.DYNAMIC_TEXTBOX, inputValue, fielName);
 	}
+
 	public void inputToTextboxDateBirth(String inputValue) {
 		waitForElementVisible(driver, CommonPageUI.DATE_OF_BIRTH_TEXTBOX);
-		sendkeyToElement(driver, CommonPageUI.DATE_OF_BIRTH_TEXTBOX,inputValue);
+		sendkeyToElement(driver, CommonPageUI.DATE_OF_BIRTH_TEXTBOX, inputValue);
 	}
+
 	public void inputToDynamicTextArea(WebDriver driver, String fielName, String inputValue) {
 		waitForElementVisible(driver, CommonPageUI.DYNAMIC_TEXTAREA, fielName);
 		sendkeyToElement(driver, CommonPageUI.DYNAMIC_TEXTAREA, inputValue, fielName);
 	}
+
 	public void clickToDynamicButton(WebDriver driver, String buttonValue) {
 		waitForElementClickable(driver, CommonPageUI.DYNAMIC_BUTTON, buttonValue);
 		clickToElement(driver, CommonPageUI.DYNAMIC_BUTTON, buttonValue);
-		
+
 	}
+
 	public void clickToDynamicRadioButton(WebDriver driver, String radioButtonValue) {
 		waitForElementClickable(driver, CommonPageUI.DYNAMIC_RADIO_BUTTON, radioButtonValue);
 		clickToElement(driver, CommonPageUI.DYNAMIC_RADIO_BUTTON, radioButtonValue);
 	}
+
 	public String getTextNewCustomerMessage(String fieldName) {
 		waitForElementVisible(driver, CommonPageUI.DYNAMIC_NEW_CUSTOMER_MESSAGE, fieldName);
 		return getElementText(driver, CommonPageUI.DYNAMIC_NEW_CUSTOMER_MESSAGE, fieldName);
 	}
-	public void clickButtonTab(String fieldName) {
+
+	public String getTextAddressMessage(String fieldName) {
+		waitForElementVisible(driver, CommonPageUI.ADDRESS_MESSAGE, fieldName);
+		return getElementText(driver, CommonPageUI.ADDRESS_MESSAGE, fieldName);
+	}
+
+	public void clickButtonTabInput(String fieldName) {
 		waitForElementVisible(driver, CommonPageUI.DYNAMIC_TEXTBOX, fieldName);
 		clickButtonTab(driver, CommonPageUI.DYNAMIC_TEXTBOX, fieldName);
+	}
+
+	public void clickButtonTabTextarea(String fieldName) {
+		waitForElementVisible(driver, CommonPageUI.DYNAMIC_TEXTAREA, fieldName);
+		clickButtonTab(driver, CommonPageUI.DYNAMIC_TEXTAREA, fieldName);
+	}
+
+	public void clickButton(WebDriver driver, String fileName) {
+		waitForElementClickable(driver, CommonPageUI.DYNAMIC_BUTTON, fileName);
+		clickToElement(driver, CommonPageUI.DYNAMIC_BUTTON, fileName);
+	}
+
+	public String getTextMessageAlert() {
+		return getAlertText(driver);
+	}
+
+	public boolean getTextValueInput(String fieldName) {
+		if (fieldName == "Address") {
+			waitForAllElementVisible(driver, CommonPageUI.DYNAMIC_TEXTAREA, fieldName);
+			if (getElementText(driver, CommonPageUI.DYNAMIC_TEXTAREA, fieldName).equals("")) {
+				return true;
+			}
+			return false;
+		} else {
+			waitForAllElementVisible(driver, CommonPageUI.DYNAMIC_TEXTBOX, fieldName);
+			if (getElementText(driver, CommonPageUI.DYNAMIC_TEXTBOX, fieldName).equals("")) {
+				return true;
+			}
+			return false;
+		}
+
+	}
+
+	public String retrievesTheEnteredValues(String rowName) {
+		if (rowName == "Birthdate") {
+			waitForElementVisible(driver, CommonPageUI.DYNAMIC_VALUE_TEXT_NEW_CUSTOMER, rowName);
+			// Đổi định dạng ngày tháng từ "dd/mm/yyyy" sang "yyyy/dd/mm"
+			SimpleDateFormat originalFormat = new SimpleDateFormat("yyyy-mm-dd");
+			SimpleDateFormat newFormat = new SimpleDateFormat("mm/dd/yyyy");
+			try {
+				// Phân tích chuổi ngày tháng từ định dạng ban đầu
+				Date date = originalFormat
+						.parse(getElementText(driver, CommonPageUI.DYNAMIC_VALUE_TEXT_NEW_CUSTOMER, rowName));
+				// Chuyển đổi sang định dạng mới
+				String formattedDate = newFormat.format(date);
+				return formattedDate;
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+		waitForElementVisible(driver, CommonPageUI.DYNAMIC_VALUE_TEXT_NEW_CUSTOMER, rowName);
+		return getElementText(driver, CommonPageUI.DYNAMIC_VALUE_TEXT_NEW_CUSTOMER, rowName);
 	}
 }
--- src/main/java/pageUIs/Guru/CommonPageUI.java
+++ src/main/java/pageUIs/Guru/CommonPageUI.java
@@ -7,5 +7,8 @@ public class CommonPageUI {
 	public static final String DYNAMIC_RADIO_BUTTON = "xpath=//input[@type='radio' and @value='%s']";
 	public static final String DYNAMIC_BUTTON = "xpath=//input[@value='%s']";
 	public static final String DYNAMIC_NEW_CUSTOMER_MESSAGE= "xpath=//td[text()='%s']/following-sibling::td/label";
+	public static final String ADDRESS_MESSAGE= "xpath=//td[text()='Address']/following-sibling::td/label";
 	public static final String DATE_OF_BIRTH_TEXTBOX = "xpath=//td[text()='Date of Birth']/following-sibling::td/input";
+	public static final String DYNAMIC_VALUE_TEXT_NEW_CUSTOMER = "xpath=//td[text()='%s']/following-sibling::td";
+
 }
